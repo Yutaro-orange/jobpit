d46c33b758c2c334202574c01f1553530d904a8c
yuchan <yutaro.tennis.orange@gmail.com>
2024-02-13 23:40:33 +0900

    打刻修正申請承認機能追加

--- jobpit/settings.py
+++ jobpit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9rr87aw-v!0%78)n))kx)t6e*^8widabf14=p3kt(9k@qik3)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
